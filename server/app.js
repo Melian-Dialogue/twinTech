7c32f8067fda38cb3c50e85c82b2285d4b3f7638
ruelngatia <ruelngatia@gmail.com>
2024-01-20 12:58:52 +0300

    another one

--- server/app.js
+++ server/app.js
@@ -8,9 +8,10 @@ import paymentRouter from './routes/PaymentRoute.js'
 const app = express()
 
 app.use(express.json())
-app.use(cors({
-    origin: ['*', 'https://mealfixer.onrender.com', 'http://localhost:4000']
-}))
+// app.use(cors({
+//     origin: ['*', 'https://mealfixer.onrender.com', 'http://localhost:4000']
+// }))
+app.use(cors())
 app.use(express.static('../front'));
 app.use(userRouter)
 app.use(paymentRouter)
